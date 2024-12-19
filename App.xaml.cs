7842c790538cbf41ee4dba2d0bd75654dcd0461c
Rojin Thapa <rojinthapa@SL-Jin-Woo.local>
2024-12-19 09:26:05 +0545

    Inital Commit

---
+++ App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace Money_Tracker;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+
+        MainPage = new MainPage();
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Money_Tracker;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+}
